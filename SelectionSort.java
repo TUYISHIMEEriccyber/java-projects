58df928c66734765288b99a2fb89846953343386
TUYISHIME Eric <116637858+TUYISHIMEEriccyber@users.noreply.github.com>
2025-02-10 23:18:20 +0200

    Add files via upload
    Sorting Algorithms

---
+++ SelectionSort.java
@@ -0,0 +1,25 @@
+
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap arr[i] and arr[minIndex]
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        System.out.println("Original array: " + java.util.Arrays.toString(arr));
+        selectionSort(arr);
+        System.out.println("Sorted array: " + java.util.Arrays.toString(arr));
+    }
+}
